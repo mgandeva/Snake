0b0c12b74347a9f230526ef3cf3ec535ce153b6b
Mihaela Gandeva <mihaela.gandeva@advance-auto.com>
2024-01-19 16:36:06 +0200

    merge main

--- src/app/models/snake.model.ts
+++ src/app/models/snake.model.ts
@@ -11,6 +11,7 @@ export class Snake {
   private headIndex: number = 0;
 
   direction: Direction = Direction.RIGHT;
+  facing: Direction = this.direction;
   body: Cell[] = INITIAL_SNAKE_COORDINATES;
 
   containsCell(cell: Cell): boolean {
@@ -23,8 +24,10 @@ export class Snake {
   move() {
     this.body = this.body.map((cell: Cell, index: number) => {
       if (index === this.headIndex) {
+        this.facing = this.direction;
         return cell.getNeighbour(this.direction);
       }
+
       return this.body[index - 1];
     });
   }
